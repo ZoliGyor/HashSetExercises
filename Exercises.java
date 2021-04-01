3be6e2e8a6e2d7bb60394516bc6548a8b5f91e68
Gyor <gyor@vdi-sccubu-003.lancs.local>
2021-04-01 14:12:42 +0100

    Did the 1st exercise

--- Exercises.java
+++ Exercises.java
@@ -1,4 +1,25 @@
+import java.util.HashSet;
+
 public class Exercises
 {
-    
+    private HashSet<String> set = new HashSet<String>();
+
+    public Exercises()
+    {
+        set.add("Matt");
+        System.out.println(set);
+    }
+
+    public void exercise1(HashSet<String> set, String element)
+    {
+        System.out.println("1st exercise: Write a Java program to append the specified element to the end of a hash set");
+        set.add(element);
+        System.out.println(set);
+        System.out.println();
+    }
+
+    public HashSet<String> getSet()
+    {
+        return set;
+    }
 }
--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ public class Main
 {    
     public static void main(String[] args)
     {
+        Exercises exercises = new Exercises();
 
+        exercises.exercise1(exercises.getSet(), "DM");
     }
 }
